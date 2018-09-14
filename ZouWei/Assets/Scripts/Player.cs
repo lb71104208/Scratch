4cff48b40d9652650ab3208132a5671230bba0e4
Li Bo <libo_d2353@virtuos.com.cn>
2018-09-14 16:34:24 +0800

    add player script

---
+++ ZouWei/Assets/Scripts/Player.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    private void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        Vector3 offset = new Vector2(h, v)*Time.deltaTime*2;
+        transform.position += offset;
+    }
+}
