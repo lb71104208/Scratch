93bbf5ce719a167859a01d2babbfecba0eab77a7
Li Bo <libo_d2353@virtuos.com.cn>
2018-09-17 14:01:03 +0800

    player animation resource

--- ZouWei/Assets/Scripts/Player.cs
+++ ZouWei/Assets/Scripts/Player.cs
@@ -2,12 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum PlayerMoveDirection
+{
+    LEFT = 0,
+    RIGHT,
+    UP,
+    DOWN,
+    NONE = -1
+}
+
 public class Player : MonoBehaviour {
+
+    private PlayerMoveDirection _moveDirection =  PlayerMoveDirection.NONE;
+    public Animator animator;
 
     private void Update()
     {
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
+
+        if(h != 0)
+        {
+            PlayerMoveDirection direction = h > 0 ? PlayerMoveDirection.RIGHT : PlayerMoveDirection.LEFT;
+            if(direction != _moveDirection)
+            {
+                _moveDirection = direction;
+                animator.SetInteger("MoveDirection", (int)direction);
+            }
+        }
+        else
+        {
+            animator.SetInteger("MoveDirection", (int)PlayerMoveDirection.NONE);
+            _moveDirection = PlayerMoveDirection.NONE;
+        }
 
         Vector3 offset = new Vector2(h, v)*Time.deltaTime*2;
         transform.position += offset;
