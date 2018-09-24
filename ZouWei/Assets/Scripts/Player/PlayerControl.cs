b0ca3f2f17d6653b85d1dc924e05771b3958a379
libo <lb71104208@163.com>
2018-09-25 00:23:30 +0800

    4 directions walk

--- ZouWei/Assets/Scripts/Player/PlayerControl.cs
+++ ZouWei/Assets/Scripts/Player/PlayerControl.cs
@@ -4,43 +4,131 @@ using UnityEngine;
 
 namespace Player
 {
-    public enum PlayerMoveDirection
+    public enum EPlayerMoveDirection
     {
-        LEFT = 0,
+        NONE = 0,
+        LEFT ,
         RIGHT,
         UP,
-        DOWN,
-        NONE = -1
+        DOWN
     }
 
     public class PlayerControl : MonoBehaviour
     {
-
-        private PlayerMoveDirection _moveDirection = PlayerMoveDirection.NONE;
         public Animator animator;
+        public Sprite idleSideSprite;
+        public Sprite idleUpSprite;
+        public Sprite idleDownSprite;
 
+        private EPlayerMoveDirection _moveDirection = EPlayerMoveDirection.NONE;
+        private Dictionary<EPlayerMoveDirection, Sprite> _idleSpriteDic;
+        private SpriteRenderer _sprite;
+
+        private void Awake()
+        {
+            SetupIdleSpriteDictionary();
+            _sprite = GetComponent<SpriteRenderer>();
+        }
+
+        private void SetupIdleSpriteDictionary()
+        {
+            _idleSpriteDic = new Dictionary<EPlayerMoveDirection, Sprite>();
+            _idleSpriteDic.Add(EPlayerMoveDirection.LEFT, idleSideSprite);
+            _idleSpriteDic.Add(EPlayerMoveDirection.RIGHT, idleSideSprite);
+            _idleSpriteDic.Add(EPlayerMoveDirection.UP, idleUpSprite);
+            _idleSpriteDic.Add(EPlayerMoveDirection.DOWN, idleDownSprite);
+        }
+        
         private void Update()
         {
             float h = Input.GetAxis("Horizontal");
             float v = Input.GetAxis("Vertical");
 
-            if (h != 0)
+            EPlayerMoveDirection direction = DetermineDirection(h, v);
+            if(direction != _moveDirection)
             {
-                PlayerMoveDirection direction = h > 0 ? PlayerMoveDirection.RIGHT : PlayerMoveDirection.LEFT;
-                if (direction != _moveDirection)
+                if(direction == EPlayerMoveDirection.NONE)
+                {
+                    OnEnterIdleState(_moveDirection);
+                    _moveDirection = EPlayerMoveDirection.NONE;
+                }
+                else
                 {
                     _moveDirection = direction;
-                    animator.SetInteger("MoveDirection", (int)direction);
+                    PlayMoveAnimation();
                 }
+                
+            }
+
+            Move(h, v);
+        }
+
+        private EPlayerMoveDirection DetermineDirection(float h, float v)
+        {
+            if(h != 0)
+            {
+                return h > 0 ? EPlayerMoveDirection.RIGHT : EPlayerMoveDirection.LEFT;
+            }
+            else if( v != 0)
+            {
+                return v > 0 ? EPlayerMoveDirection.UP : EPlayerMoveDirection.DOWN;
             }
             else
             {
-                animator.SetInteger("MoveDirection", (int)PlayerMoveDirection.NONE);
-                _moveDirection = PlayerMoveDirection.NONE;
+                return EPlayerMoveDirection.NONE;
             }
+        }
 
+        private void PlayMoveAnimation()
+        {
+            //leave face state
+            animator.SetInteger("FaceDirection", 0);
+
+            if(_moveDirection == EPlayerMoveDirection.LEFT)
+            {
+                animator.SetInteger("MoveDirection", (int)EPlayerMoveDirection.RIGHT);
+            }
+            else
+            {
+                animator.SetInteger("MoveDirection", (int)_moveDirection);
+            }
+            
+            if(_moveDirection == EPlayerMoveDirection.LEFT)
+            {
+                _sprite.flipX = true;
+            }
+            else
+            {
+                _sprite.flipX = false;
+            }
+        }
+
+        private void Move(float h, float v)
+        {
             Vector3 offset = new Vector2(h, v) * Time.deltaTime * 2;
             transform.position += offset;
+        }
+
+        private void OnEnterIdleState(EPlayerMoveDirection faceDir)
+        {
+            if (faceDir == EPlayerMoveDirection.LEFT)
+            {
+                faceDir = EPlayerMoveDirection.RIGHT;
+                _sprite.flipX = true;
+            }
+            else
+            {
+                _sprite.flipX = false;
+            }
+
+            //leave move state
+            animator.SetInteger("MoveDirection", 0);
+
+            animator.SetInteger("FaceDirection", (int)faceDir);
+            Debug.Log("faceDir" + faceDir);
+            //_sprite.sprite = _idleSpriteDic[faceDir];
+            
+            //animator.StopPlayback();
         }
     }
 }
